19004b1c600b332fc26f7ea936d5ea3bb4467bfb
Andreea Musca <andreeamusca382@gmail.com>
2023-12-09 19:52:45 +0200

    Create add course page

--- KnowledgeNexusModels/Models/Course.cs
+++ KnowledgeNexusModels/Models/Course.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson;
+﻿using System.Collections.Generic;
+using MongoDB.Bson;
 
 namespace KnowledgeNexusModels.Models
 {
@@ -9,5 +10,26 @@ namespace KnowledgeNexusModels.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public string Author { get; set; }
+        public List<Category> Categories { get; set; } = new List<Category>();
+        public List<CourseContent> Contents { get; set; } = new List<CourseContent>();
+    }
+
+    public class Category
+    {
+        public string Name { get; set; }
+        public string ParentCategory { get; set; } // Parent category name
+    }
+
+    public class CourseContent
+    {
+        public ContentType Type { get; set; }
+        public string Url { get; set; }
+    }
+
+    public enum ContentType
+    {
+        Audio,
+        Video,
+        PDF,
     }
 }

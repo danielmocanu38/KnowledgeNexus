ca73c06d3e023b42b4d2fb68d009a6d2c6e01149
daniel mocanu <daniel-m.mocanu@student.unitbv.ro>
2023-12-09 22:32:49 +0200

    Updated visual elements

--- KnowledgeNexusModels/Models/Course.cs
+++ KnowledgeNexusModels/Models/Course.cs
@@ -5,9 +5,9 @@ namespace KnowledgeNexusModels.Models
     public class Course
     {
         public ObjectId Id { get; set; }
-        public string ImageUrl { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Author { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
     }
 }

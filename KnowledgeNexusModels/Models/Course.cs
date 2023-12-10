bc4b6a9a400547818a790dec427c8dec18dbd6af
daniel mocanu <daniel-m.mocanu@student.unitbv.ro>
2023-12-10 02:23:06 +0200

    Updated CourseCard footer

--- KnowledgeNexusModels/Models/Course.cs
+++ KnowledgeNexusModels/Models/Course.cs
@@ -6,24 +6,24 @@ namespace KnowledgeNexusModels.Models
     public class Course
     {
         public ObjectId Id { get; set; }
-        public string ImageUrl { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Author { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
         public List<Category> Categories { get; set; } = new List<Category>();
         public List<CourseContent> Contents { get; set; } = new List<CourseContent>();
     }
 
     public class Category
     {
-        public string Name { get; set; }
-        public string ParentCategory { get; set; } // Parent category name
+        public string Name { get; set; } = string.Empty;
+        public string ParentCategory { get; set; } = string.Empty; // Parent category name
     }
 
     public class CourseContent
     {
         public ContentType Type { get; set; }
-        public string Url { get; set; }
+        public string Url { get; set; } = string.Empty;
     }
 
     public enum ContentType
